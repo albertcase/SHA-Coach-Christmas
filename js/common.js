3ce75a089396fe0e0a67d51c3d8e9e9ae12d4b0a
Evelyn Zhang <evelyn.zhang@samesamechina.com>
2015-12-08 16:37:05 +0800

    animate page

--- js/common.js
+++ js/common.js
@@ -22,7 +22,8 @@ function isWeiXin() {
 jQuery(document).ready(function($){
 
 	gotoPin(0);
-	
+
+	//register shake
 	var testShake = new Shake({
 		threshold: 10, //default velocity threshold for shake to register
 		timeout: 1000 //default interval between events
@@ -32,7 +33,45 @@ jQuery(document).ready(function($){
 	function shakeEventDidOccur () {
 
 		//put your own code here etc.
-		console.log(888);
+		if($('.pin-2').hasClass('current')){
+			console.log('start api');
+		}
 	}
+
+//	click buttons==>
+	$('.buttons').on('click', function(){
+		if($(this).hasClass('p1-3')){
+			console.log(1);
+			//go shake page
+			gotoPin(1);
+		}else if($(this).hasClass('p3-5')){
+			//go form page
+			gotoPin(4);
+		}else if($(this).hasClass('btn-submit')){
+			//submit the form
+			//var name =
+			//service.formSubmit(name,number,function(){
+            //
+			//});
+			if (service.formValidation()){
+				console.log('validated');
+			}
+		}else if($(this).hasClass('p4-5')){
+			//go shake page
+			gotoPin(1);
+		}
+	});
+
+
+
+
+
+//test
+	$('.p2-3').on('click',function(){
+		gotoPin(2);
+	});
+
+
+
 
 });
--- js/service.js
+++ js/service.js
@@ -30,13 +30,13 @@ var service = function(){
             }
         })
     };
-    var formSubmit = function(name,address,phone,callback){
+    var formSubmit = function(name,phone,callback){
         ajaxPop.show();
         $.ajax({
             url:path+'/shake/info/json',
             type:'POST',
             dataType:'json',
-            data:{name:name,address:address,phone:phone},
+            data:{name:name,phone:phone},
             success:function(data){
                 ajaxPop.hide();
                 return callback(data);
@@ -44,9 +44,56 @@ var service = function(){
         })
     };
 
+    var errorMsg = {
+        add:function(ele,msg){
+            if(!ele.find('.error').length){
+                ele.append('<div class="error">'+msg+'</div>');
+            }else{
+                ele.find('.error').html(msg);
+            }
+        },
+        remove:function(ele){
+            if(ele.find('.error').length){
+                ele.find('.error').remove();
+            }
+        }
+    };
+
+    var formValidation = function(){
+
+        var validate = true;
+        if(!$('.input-name').val()){
+            errorMsg.add($('.input-name').parent(),'姓名不能为空');
+            validate = false;
+        }else{
+            errorMsg.remove($('.input-name').parent());
+        }
+
+        if(!$('.input-phone').val()){
+            errorMsg.add($('.input-phone').parent(),'手机号码不能为空');
+            validate = false;
+        }else{
+            var reg=/^1\d{10}$/;
+            if(!(reg.test($('.input-phone').val()))){
+                validate = false;
+                errorMsg.add($('.input-phone').parent(),'手机号格式错误，请重新输入');
+            }else{
+                errorMsg.remove($('.input-phone').parent());
+            }
+
+        }
+
+        if(validate){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     return{
         path:path,
         isPrize:isPrize,
+        formValidation:formValidation,
         formSubmit:formSubmit
         //fruitDrop:fruitDrop,
         //animate:animate
