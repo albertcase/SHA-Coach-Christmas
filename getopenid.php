ab207808f57725712041a2799c790fae8cd1a79a
Demon Zhang <demon.zhang@samesamechina.com>
2015-12-13 17:46:13 +0800

    no message

--- getopenid.php
+++ getopenid.php
@@ -7,7 +7,10 @@ $db = Pdb::getDb();
 $_SESSION['openid'] = $_GET['openid'];
 $user = $db->getRow("SELECT * FROM `coach_xmas_info` WHERE `openid` = " . $db->quote($_SESSION['openid']), true);
 if (!$user) {
-	$sql = "INSERT INTO `coach_xmas_info` SET `openid` = " . $db->quote($_SESSION['openid']) . ",status = 1";
+	$info = file_get_contents("http://api.curio.im/v2/wx/users/".$_SESSION['openid']."?access_token=08ecb2077e158fd621a1f175e22442e8");
+	$info = json_decode($info, true);
+	$basename = json_encode(array('name' => $info['data']['nickname']));
+	$sql = "INSERT INTO `coach_xmas_info` SET `openid` = " . $db->quote($_SESSION['openid']) . ",nickname = " . $db->quote($info['data']['nickname']) . ",basename = " . $db->quote($basename) . ",headimgurl = " . $db->quote($info['data']['headimgurl']) . " ,status = 1";
 	$db->execute($sql);
 	$_SESSION['user_id'] = $db->lastInsertId;
 } else {
