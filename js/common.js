20d73638b14b86a7b7634ae25c218e82ca78c15a
Evelyn Zhang <evelyn.zhang@samesamechina.com>
2015-12-16 12:24:54 +0800

    baidu tracking

--- js/common.js
+++ js/common.js
@@ -132,13 +132,16 @@ jQuery(document).ready(function($){
 					//1 礼券
 					gotoPin(2);
 					$('.pin-3').removeClass('getcoupon');
+					_hmt.push(['_trackEvent', '摇一摇', 'shake', '第二次摇奖的数量']);
 				}else if(data.code==2){
 					//2 卡包
 					gotoPin(2);
 					$('.pin-3').addClass('getcoupon');
+					_hmt.push(['_trackEvent', '摇一摇', 'shake', '第一次摇奖的数量']);
 				}else if(data.code==3){
 					//3 未中奖
 					gotoPin(3);
+					_hmt.push(['_trackEvent', '摇一摇', 'shake', '第一次摇奖的数量']);
 				}else if(data.code==4){
 					//无中奖权限
 					$('.share').addClass('show');
@@ -156,53 +159,50 @@ jQuery(document).ready(function($){
 //	click buttons==>
 	$('.buttons').on('click', function(){
 		if($(this).hasClass('p1-3')){
-			//go shake page
+			//go shake page 我要摇奖
 			gotoPin(1);
-			if(parseInt(CANSHAKE)>0){
-				//enableShake = true;
-			}else{
-				//enableShake = false;
+			if(!parseInt(CANSHAKE)>0){
 				$('.share').addClass('show');
 			}
+			_hmt.push(['_trackEvent', 'buttons', 'click', '我要摇奖']);
 		}else if($(this).hasClass('gocoupon')){
+
+			if($(this).hasClass('coupon-1')){
+				//	领取圣诞卡券1
+				_hmt.push(['_trackEvent', 'buttons', 'click', '领取圣诞卡券1']);
+			}else if($(this).hasClass('coupon-2')){
+				//	领取圣诞卡券2
+				_hmt.push(['_trackEvent', 'buttons', 'click', '领取圣诞卡券2']);
+			}
 			addCard(1);
 		}else if($(this).hasClass('p3-5')){
-			//go form page
+			//go form page 留下你的获奖信息
 			gotoPin(4);
+			_hmt.push(['_trackEvent', 'buttons', 'click', '留下你的获奖信息']);
 		}else if($(this).hasClass('btn-submit')){
 			if (service.formValidation()){
-				console.log('validated');
 				//submit the form
 				var name =$('.input-name').val();
 				var number =$('.input-phone').val();
 				service.formSubmit(name,number,function(){
-					console.log('success');
 					//go to success page
 					gotoPin(5);
+					_hmt.push(['_trackEvent', 'buttons', 'click', '提交']);
 				});
 			}
 		}else if($(this).hasClass('p4-5')){
-			//go shake page
-			//$('.share').addClass('show');
-			service.addChance(function(data){
-				if(data.code){
-					alert('获得一次抽奖机会');
-					gotoPin(1);
-				}else{
-					alert('未登录');
-				}
-			});
+			//go shake page 再摇一次
+			$('.share').addClass('show');
+			_hmt.push(['_trackEvent', 'buttons', 'click', '再摇一次']);
 		}else if($(this).hasClass('p7-btn')){
 			gotoPin(0);
 		}
 	});
 
-//	go first page
-	$('.back').on('click', function(){
-		gotoPin(0);
-	});
+	//活动规则
 	$('.p1-5').on('click', function(){
 		gotoPin(6);
+		_hmt.push(['_trackEvent', 'buttons', 'click', '活动细则']);
 	});
 
 
