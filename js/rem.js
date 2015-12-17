ac0c9a758b31db11e5197181c8d6f2067893adde
Evelyn Zhang <evelyn.zhang@samesamechina.com>
2015-12-17 16:48:46 +0800

    remove use console

--- js/rem.js
+++ js/rem.js
@@ -5,13 +5,10 @@
 			var clientWidth = docEl.clientWidth;
 			var clientHeight = window.innerHeight;
 			if (!clientWidth) return;
-			console.log(clientWidth/clientHeight+''+750/1116);
 			if(clientWidth/clientHeight > 750/1116){
-				console.log(1);
 				docEl.style.fontSize = 50 * (clientHeight / 558) + 'px';
 				$('.wrapper').addClass('landscape');
 			}else{
-				console.log(2);
 				$('.wrapper').removeClass('landscape');
 				docEl.style.fontSize = 50 * (clientWidth / 375) + 'px';
 			}
