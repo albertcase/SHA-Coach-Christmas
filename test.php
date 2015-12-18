7cb9d373dee8033effbc79340fb3096b501c93e1
Demon Zhang <demon.zhang@samesamechina.com>
2015-12-18 12:54:12 +0800

    no message

--- test.php
+++ test.php
@@ -1,21 +1,30 @@
 <?php
+$tag = $_GET['tag'];
 require_once dirname(__FILE__) . "/conf/config.php";
 $user = new stdClass();
 $user->uid = 1;
 $user->openid = 'oKCDxjvvyfIqg0lKXNJrc0szWzSg';
 $user->lottery = 0;
 $user->status = 1;
-$wechatAPI = new WechatAPI();
-$re = $wechatAPI->isUserSubscribed($user->openid);
-//$re = 1;
+if ($tag ==1) {
+    $wechatAPI = new WechatAPI();
+    $re = $wechatAPI->isUserSubscribed($user->openid);
+} else {
+    $re = 1;
+}
 if (!$re) {
     print file_get_contents(TEMPLATE_ROOT . 'qrcode.html');
     exit;
 }
 print file_get_contents(TEMPLATE_ROOT . 'home.html');
 print '<script>var CANSHAKE="'.$user->status.'";</script>';
-$cardList = $wechatAPI->cardList();
-print '<script>var cardListJSON = '.json_encode($cardList).'</script>';
+if ($tag == 1) {
+    $cardList = $wechatAPI->cardList();
+    print '<script>var cardListJSON = '.json_encode($cardList).'</script>';
+} else {
+    print '<script>var cardListJSON = {}</script>';
+}
+
 $RedisAPI = new RedisAPI();
 $list = $RedisAPI->getLotteryList();
 if (!$list) {
